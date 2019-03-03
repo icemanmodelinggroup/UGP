b78231687b3e04a11152602a17cc590cb3e090e5
Jacob Wolfe <jacob.wolfe@umontana.edu>
2019-03-02 17:43:12 -0700

    Signed-off-by: Jacob Wolfe <jacob.wolfe@umontana.edu>

--- UGPFrontEnd/scripts/script.js
+++ UGPFrontEnd/scripts/script.js
@@ -29,13 +29,7 @@ function retrieveData(latitude, longitude){
 	values = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
 	j = 8;
 
-/* 	$.post("test.asp", {lat: "d", lon: "S"},   
-	function(data, status){
-		alert("Data: " + data + "\nStatus: " + status);
-	}); 
-	 */
-	 
-	//Simulates creating values  - Real code to communicate w/server will go here
+	//Insert call to AJAX function
 	
 	for(i = 0; i < 6; i++){
 		values[i] = ((longitude+j-2*i) - (1/5)*latitude*(i**2 - (j+i)**(.3)));
@@ -86,37 +80,56 @@ function printData(point, numRows){
 }
 
 //This function loads the Point dropdown list with values
-function loadPointDD(line, numPoints){
-	//TODO: RENAME some variables
+function loadPointDD(line, numPoints, oldPoints){
+	
 	pointList = document.getElementById('pointList');
+	
+	//Remove old points
+	for(var i = 0; i < oldPoints; i++){
+		pointList.remove(i);
+	}
+	
+	//Iterate through points in line and add to dropdown
 	for(var i = 0; i < numPoints; i++) {
+		
         var newOption = document.createElement('option');
-        newOption.text = "Point: " + line.pointList[i].name;
+        newOption.text = "(" + line.pointList[i].name + ")";
 		newOption.value = i;
 		newOption.point = line.pointList[i];
-		pointList.add(newOption, 0);
+		pointList.add(newOption, 0);	
     }
+	
 	return;
 }
 
-function loadLineDD(lines, numLines){
-	//TODO: Fix this. 
-	/*
+//This function loads the Line dropdown list with values
+function loadLineDD(line){
+
+ 	var label = "";
+	
 	lineList = document.getElementById('lineList');
-	for(var i = 0; i < numPoints; i++) {
-        var newOption = document.createElement('option');
-        newOption.text = "Point: " + line.pointList[i].name;
-		newOption.value = i;
-		newOption.point = line.pointList[i];
-		lineList.add(newOption, 0);
-    }
-	return;
-	*/
+	
+	//Add line to dropdown
+    var newOption = document.createElement('option');
+	if(line.numPoints > 1){
+		label = "Line: ";
+	}
+	else{
+		label = "Point: ";
+	}
+    newOption.text = label + line.name;
+	newOption.line = line;
+	lineList.add(newOption, 0);
+   
+	return; 
+
 }
 
+//Clears all rows of the console table
 function clearTable()
 {
 	var table = document.getElementById("consoletable");
+	
 	//Delete the previous values
 	var currNumRows = table.rows.length;
 	for (var i = 1; i < currNumRows; i++){
@@ -135,20 +148,38 @@ $(function(){
 	var lat = 0.0;
 	var lon = 0.0;
 	var list = "";
-	var mode = 0;
+	var oldPoints = 0;
 	var num = 0;
 	var i;
 
+	//Event handler for point dropdown
 	$('#pointList').change(function () {
-		//TODO: Does this do exactly what we want? I have no idea. May need to change
+
 		clearTable();
+		
+		//Grab selected point
 		var optionSelected = $(this).find("option:selected");
-		selectedPoint = Object.values(optionSelected)[0].point;
+		selectedPoint = Object.values(optionSelected)[0].point
 		printData(selectedPoint,numRows);
 	 });
+	 
+	 //Event handler for line dropdown
 	$('#lineList').change(function () {
-		//TODO: Add functionality to adding lines
+
+		clearTable();
+		
+		//Grab selected line
 		var optionSelected = $(this).find("option:selected");
+		
+		//Iterate through line and print points
+		for(i = 0; i < Object.values(optionSelected)[0].line.numPoints; i++){
+			selectedPoint = Object.values(optionSelected)[0].line.pointList[i];
+			printData(selectedPoint,numRows);
+		}
+
+		//Increment numPoints and reload point dropdown
+		numPoints = Object.values(optionSelected)[0].line.numPoints;
+		loadPointDD(Object.values(optionSelected)[0].line, numPoints, oldPoints);
 
 	 });
 	//Event handler for go button
@@ -168,16 +199,17 @@ $(function(){
 		//Create point with data from server & add to points list
 		var point = retrieveData(lat, lon);
 		points.push(point);
+		oldPoints = numPoints;
 		numPoints = 1;
 
 		//Create a line (with a single point in it, in this case)
- 		var line = new Line(points, 1, ("Point " + points[0].name));
+ 		var line = new Line(points, 1, (points[0].name));
 		lines.push(line);
 		numLines++;
 		
 		//Load the point and line dropdowns
-		loadPointDD(line, numPoints);
-		loadLineDD(lines, numLines);
+		loadPointDD(line, numPoints, oldPoints);
+		loadLineDD(line);
 		
 		//Print the point data to the console
 		printData(point, numRows);
